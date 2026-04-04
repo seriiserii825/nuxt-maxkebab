8ad821f9645cd0338c626a73bdfc3adc901e907d
seriiserii825 <seriiburduja@gmail.com>
2026-04-04 15:02:28 +0300

    feat: i home response ts

--- app/interfaces/IHomeResponse.ts
+++ app/interfaces/IHomeResponse.ts
@@ -8,3 +8,33 @@ export interface ISlider {
   ialoveni_slider: string[];
   ialoveni_slider_mobile: string[];
 }
+
+export interface ICatalog {
+  id: number;
+  name: string;
+  slug: string;
+  has_children: boolean;
+  children: ICatalogChild[];
+}
+
+export interface ICatalogChild {
+  id: number;
+  name: string;
+  slug: string;
+  image: null | string;
+  products: IProduct[];
+}
+
+export interface IProduct {
+  id: number;
+  title: string;
+  slug: string;
+  permalink: string;
+  image: string;
+  description: string;
+  sku: string;
+  regular_price: string;
+  sale_price: null | string;
+  is_on_sale: boolean;
+  has_options: boolean;
+}
