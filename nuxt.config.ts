e79ff2c60f7183fe330df321579f30cd4a91478a
seriiserii825 <seriiburduja@gmail.com>
2026-04-04 08:45:12 +0300

    feat: move to app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  srcDir: "src/app",
   vite: {
     optimizeDeps: {
       include: ["@vue/devtools-core", "@vue/devtools-kit"],
