7769ba029377efafd91156443cc93028b8327cad
seriiserii825 <seriiburduja@gmail.com>
2026-04-04 09:04:53 +0300

    feat: i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,14 @@ export default defineNuxtConfig({
     },
   },
   css: ["@/assets/scss/my.scss"],
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    locales: [
+      { code: "en", name: "English" },
+      { code: "ru", name: "Русский" },
+      { code: "ro", name: "Română" },
+    ],
+    defaultLocale: "en",
+    strategy: "prefix", // /en, /ru, /ro
+  },
 });
