8469fe8d0e552a5f3c4dd92a80a36e285fb25b23
seriiserii825 <seriiburduja@gmail.com>
2026-04-04 13:51:59 +0300

    feat: default show error

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 const { data: menu, error } = await useFetch("/api/menu");
-if (error.value) showError(error.value);
+if (error.value) {
+  showError({
+    statusCode: error.value.statusCode ?? 500,
+    message: error.value.data?.message ?? error.value.message,
+  });
+}
 </script>
 
 <template>
