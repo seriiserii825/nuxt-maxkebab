b39c4c37161cf5ae93abe8d4afb83fdd04cbbb9d
serii burduja <seriiburduja@gmail.com>
2026-04-06 16:59:44 +0300

    feat: city select without popup

--- app/components/UI/CitySelect.vue
+++ app/components/UI/CitySelect.vue
@@ -7,31 +7,31 @@ const { currentCity, cities } = storeToRefs(cityStore);
 
 const is_open_dropdown = ref(false);
 
-function handleItemClick(index: number, slug: ICity["slug"]) {
+function handleItemClick(index: number) {
   cityStore.setCityIndex(index);
   is_open_dropdown.value = false;
-  if (slug === "chisinau") {
-    cityStore.setPopupIsActive(true);
-  }
 }
 </script>
 
 <template>
   <ul class="city-select">
     <li class="city-select__item">
-      <div class="city-select__title" @click="is_open_dropdown = !is_open_dropdown">
+      <div
+        class="city-select__title"
+        v-if="currentCity"
+        @click="is_open_dropdown = !is_open_dropdown">
         <span>{{ t(currentCity.text) }}</span>
         <small>{{ currentCity.address }}</small>
         <IconDown />
       </div>
-      <ul class="city-select__list" :class="{ active: is_open_dropdown }">
+      <ul class="city-select__list" :class="{ active: is_open_dropdown }" v-if="currentCity">
         <li
           v-for="(city, index) in cities"
           v-show="city.slug !== currentCity.slug"
           :key="city.slug"
           class="city-select__item"
           :data-city-select="city.slug"
-          @click="handleItemClick(index, city.slug)">
+          @click="handleItemClick(index)">
           {{ t(city.text) }}
         </li>
       </ul>
