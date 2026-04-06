9d8baf45d589c59a9050612b715b2790d054c4b5
serii burduja <seriiburduja@gmail.com>
2026-04-06 17:21:44 +0300

    feat: city storage persist

--- app/stores/useCityStore.ts
+++ app/stores/useCityStore.ts
@@ -2,25 +2,31 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { cities_data } from "~/data/cities";
 import type { ICity } from "~/interfaces/ICity";
-export const useCityStore = defineStore("city", () => {
-  const popup_is_active = ref(false);
-  const city_index = ref(0);
-  const cities = ref<ICity[]>(cities_data);
+export const useCityStore = defineStore(
+  "city",
+  () => {
+    const popup_is_active = ref(false);
+    const city_index = ref(0);
+    const cities = ref<ICity[]>(cities_data);
 
-  const currentCity = computed<ICity | undefined>(() => cities.value[city_index.value]);
+    const currentCity = computed<ICity | undefined>(() => cities.value[city_index.value]);
 
-  function setCityIndex(index: number) {
-    city_index.value = index;
-  }
+    function setCityIndex(index: number) {
+      city_index.value = index;
+    }
 
-  function setPopupIsActive(isActive: boolean) {
-    popup_is_active.value = isActive;
-  }
-  return {
-    cities,
-    setCityIndex,
-    currentCity,
-    popup_is_active,
-    setPopupIsActive,
-  };
-});
+    function setPopupIsActive(isActive: boolean) {
+      popup_is_active.value = isActive;
+    }
+    return {
+      cities,
+      setCityIndex,
+      currentCity,
+      popup_is_active,
+      setPopupIsActive,
+    };
+  },
+  {
+    persist: true,
+  },
+);
