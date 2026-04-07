4e480439e1b151207ae7f85d38e48f29ca1b79f2
serii burduja <seriiburduja@gmail.com>
2026-04-07 11:47:37 +0300

    feat: need to change products section

--- app/components/Section/SectionProductsSection.vue
+++ app/components/Section/SectionProductsSection.vue
@@ -6,17 +6,25 @@ defineProps({
     type: Object as PropType<ICatalog>,
     required: true,
   },
+  gradients: {
+    type: Object as PropType<IGradientsBg>,
+    required: true,
+  },
 });
 </script>
 
 <template>
-  <div class="products-section">
+  <div class="products-section" :style="{ background: bg }">
     <img class="rndAnim bg-image bg-image--1" src="" alt="pizza" loading="lazy" />
     <img class="rndAnim bg-image bg-image--2" src="" alt="pizza" loading="lazy" />
 
-    <div class="products-section__inner">
-      <div class="container">
-        <div v-for="child in catalog.children" :key="child.id">
+    <div
+      v-for="(child, index) in catalog.children"
+      :key="child.id"
+      :style="{ background: gradients.gradients[index] }"
+      class="products-section__child">
+      <div class="products-section__inner">
+        <div class="container">
           <div class="products-section__header">
             <h2 class="products-section__title">
               <span>{{ child.name }}</span>
--- app/components/Section/SectionProductsSections.vue
+++ app/components/Section/SectionProductsSections.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts">
 import type { ICatalog } from "~/interfaces/IHomeResponse";
 
-
 defineProps<{
   catalog: ICatalog[];
 }>();
+
+const gradients_bg: IGradientsBg[] = [
+  {
+    gradients: [
+      "linear-gradient(180deg,#fffef1 80.21%,#f1fff5 100%)",
+      "linear-gradient(180deg,#f1fff5 76.56%,#fcf9f4 100%)",
+      "#fcf9f4",
+      "linear-gradient(180deg,#fcf9f4 0%,#d9edff 100%)",
+      "linear-gradient(180deg,#d9edff 33%,#f1fff5 100%)",
+      "linear-gradient(180deg,#f1fff5 76.56%,#fcf9f4 100%)",
+    ],
+  },
+  {
+    gradients: [
+      "linear-gradient(180deg,#fffef1 80.21%,#f1fff5 100%)",
+      "linear-gradient(180deg,#f1fff5 76.56%,#fcf9f4 100%)",
+      "#fcf9f4",
+    ],
+  },
+  {
+    gradients: ["linear-gradient(180deg,#fffef1 80.21%,#f1fff5 100%)"],
+  },
+];
 </script>
 
 <template>
-  <SectionProductsSection v-for="ctg in catalog" :key="ctg.id" :catalog="ctg" />
+  <SectionProductsSection
+    v-for="(ctg, index) in catalog"
+    :key="ctg.id"
+    :catalog="ctg"
+    :gradients="gradients_bg[index] || { gradients: [] }" />
 </template>
