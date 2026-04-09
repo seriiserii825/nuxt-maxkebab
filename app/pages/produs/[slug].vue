34f9d9e52710cf74d5df5959f2172d81dc922bd1
seriiserii825 <seriiburduja@gmail.com>
2026-04-09 21:14:12 +0300

    feat: format vue

--- app/pages/produs/[slug].vue
+++ app/pages/produs/[slug].vue
@@ -1,90 +1,90 @@
 <script setup lang="ts">
-import { useSingleProductStore } from "~/stores/useSingleProductStore";
+  import { useSingleProductStore } from "~/stores/useSingleProductStore";
 
-const store = useSingleProductStore();
-store.init(135);
+  const store = useSingleProductStore();
+  store.init(135);
 
-const breadcrumbs = [
-  { label: "Acasă", to: "/" },
-  { label: "Combo", to: "/#meniuri" },
-  { label: "Menu Kebab standart" },
-];
+  const breadcrumbs = [
+    { label: "Acasă", to: "/" },
+    { label: "Combo", to: "/#meniuri" },
+    { label: "Menu Kebab standart" },
+  ];
 
-const bautura = [
-  { value: "Coca-Cola", label: "Coca-Cola", checked: true },
-  { value: "Sprite", label: "Sprite" },
-  { value: "Fanta", label: "Fanta" },
-];
+  const bautura = [
+    { value: "Coca-Cola", label: "Coca-Cola", checked: true },
+    { value: "Sprite", label: "Sprite" },
+    { value: "Fanta", label: "Fanta" },
+  ];
 
-const sos = [
-  { value: "Ketchup", label: "Ketchup", checked: true },
-  { value: "Maioneza cu usturoi", label: "Maioneza cu usturoi" },
-];
+  const sos = [
+    { value: "Ketchup", label: "Ketchup", checked: true },
+    { value: "Maioneza cu usturoi", label: "Maioneza cu usturoi" },
+  ];
 
-const adaosuri = [
-  { name: "adaos_carne", value: "Carne", label: "Carne", price: 15 },
-  { name: "adaos_cascaval", value: "Cascaval", label: "Cascaval", price: 20 },
-  { name: "adaos_ciuperci", value: "Ciuperci", label: "Ciuperci", price: 20 },
-  { name: "adaos_masline", value: "Masline", label: "Masline", price: 15 },
-  { name: "adaos_lavas", value: "Lavas", label: "Lavas", price: 5 },
-];
+  const adaosuri = [
+    { name: "adaos_carne", value: "Carne", label: "Carne", price: 15 },
+    { name: "adaos_cascaval", value: "Cascaval", label: "Cascaval", price: 20 },
+    { name: "adaos_ciuperci", value: "Ciuperci", label: "Ciuperci", price: 20 },
+    { name: "adaos_masline", value: "Masline", label: "Masline", price: 15 },
+    { name: "adaos_lavas", value: "Lavas", label: "Lavas", price: 5 },
+  ];
 
-const adaosuriSosuri = [
-  {
-    id: "sos_picant",
-    name: "sos_picant",
-    label: "Sos picant",
-    price: 10,
-    desc: "50 g · Max: 5",
-    min: 0,
-    max: 5,
-  },
-  {
-    id: "maioneza_usturoi",
-    name: "maioneza_usturoi",
-    label: "Maioneza cu usturoi",
-    price: 12,
-    desc: "50 g · Max: 5",
-    min: 0,
-    max: 5,
-  },
-  {
-    id: "ketchup_sos",
-    name: "ketchup_sos",
-    label: "Ketchup",
-    price: 12,
-    desc: "50 g · Max: 5",
-    min: 0,
-    max: 5,
-  },
-  {
-    id: "sos_cascaval",
-    name: "sos_cascaval",
-    label: "Sos cascaval",
-    price: 15,
-    desc: "50 g · Max: 5",
-    min: 0,
-    max: 5,
-  },
-  {
-    id: "mustar",
-    name: "mustar",
-    label: "Mustar",
-    price: 10,
-    desc: "50 g · Max: 5",
-    min: 0,
-    max: 5,
-  },
-  {
-    id: "mustar_american",
-    name: "mustar_american",
-    label: "Mustar american",
-    price: 10,
-    desc: "50 g · Max: 5",
-    min: 0,
-    max: 5,
-  },
-];
+  const adaosuriSosuri = [
+    {
+      id: "sos_picant",
+      name: "sos_picant",
+      label: "Sos picant",
+      price: 10,
+      desc: "50 g · Max: 5",
+      min: 0,
+      max: 5,
+    },
+    {
+      id: "maioneza_usturoi",
+      name: "maioneza_usturoi",
+      label: "Maioneza cu usturoi",
+      price: 12,
+      desc: "50 g · Max: 5",
+      min: 0,
+      max: 5,
+    },
+    {
+      id: "ketchup_sos",
+      name: "ketchup_sos",
+      label: "Ketchup",
+      price: 12,
+      desc: "50 g · Max: 5",
+      min: 0,
+      max: 5,
+    },
+    {
+      id: "sos_cascaval",
+      name: "sos_cascaval",
+      label: "Sos cascaval",
+      price: 15,
+      desc: "50 g · Max: 5",
+      min: 0,
+      max: 5,
+    },
+    {
+      id: "mustar",
+      name: "mustar",
+      label: "Mustar",
+      price: 10,
+      desc: "50 g · Max: 5",
+      min: 0,
+      max: 5,
+    },
+    {
+      id: "mustar_american",
+      name: "mustar_american",
+      label: "Mustar american",
+      price: 10,
+      desc: "50 g · Max: 5",
+      min: 0,
+      max: 5,
+    },
+  ];
 </script>
 
 <template>
@@ -97,27 +97,39 @@ const adaosuriSosuri = [
           <ProductInfo
             title="Menu Kebab standart"
             description="Kebab standart; Cartofi pai; Bautura; Sos"
-            :price="store.totalPrice" />
+            :price="store.totalPrice"
+          />
 
           <ProductActions
             :product-id="1"
             title="Menu Kebab standart"
-            image="https://maxkebab.md/wp-content/uploads/2025/11/meniu-kebab-standart-600x900.jpg" />
+            image="https://maxkebab.md/wp-content/uploads/2025/11/meniu-kebab-standart-600x900.jpg"
+          />
 
           <div class="single-product__form">
             <ProductOptionGroupRadio
               title="Bautura"
               :required="true"
               name="bautura"
-              :options="bautura" />
-            <ProductOptionGroupRadio title="Sos" :required="true" name="sos" :options="sos" />
+              :options="bautura"
+            />
+            <ProductOptionGroupRadio
+              title="Sos"
+              :required="true"
+              name="sos"
+              :options="sos"
+            />
             <ProductOptionGroupTextarea
               title="Mențiuni"
               id="comentariu"
               name="comentariu"
-              label="Comentariu" />
+              label="Comentariu"
+            />
             <ProductOptionGroupCheckbox title="Adaosuri" :options="adaosuri" />
-            <ProductOptionGroupStepper title="Adaosuri sosuri" :items="adaosuriSosuri" />
+            <ProductOptionGroupStepper
+              title="Adaosuri sosuri"
+              :items="adaosuriSosuri"
+            />
           </div>
         </div>
 
@@ -125,7 +137,8 @@ const adaosuriSosuri = [
           src="https://maxkebab.md/wp-content/uploads/2025/11/meniu-kebab-standart-600x900.jpg"
           alt="Menu Kebab standart"
           :width="600"
-          :height="900" />
+          :height="900"
+        />
       </div>
     </div>
   </div>
