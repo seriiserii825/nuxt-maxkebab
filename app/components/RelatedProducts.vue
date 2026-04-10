e1a3e5658e99c947e0756be3049f31d6ba640d9a
seriiserii825 <seriiburduja@gmail.com>
2026-04-10 19:30:36 +0300

    feat: start related products

--- app/components/RelatedProducts.vue
+++ app/components/RelatedProducts.vue
@@ -11,6 +11,17 @@
 
 <template>
   <div class="related-products">
-    <ProductCard v-for="product in products" :key="product.id" :product="product" />
+    <div class="related-products__grid">
+      <ProductCard v-for="product in products" :key="product.id" :product="product" />
+    </div>
   </div>
 </template>
+<style lang="scss">
+  .related-products {
+    &__grid {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+      gap: 32px;
+    }
+  }
+</style>
