94557c05c1f2fc7223d15cbdabdd4eab5f015b63
seriiserii825 <seriiburduja@gmail.com>
2026-04-10 20:40:52 +0300

    feat: oferte page and api

--- app/pages/oferte.vue
+++ app/pages/oferte.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const { locale } = useI18n();
+  const { data, error } = await useFetch("/api/offers", {
+    query: { lang: locale },
+    watch: [locale],
+  });
+
+  if (error.value) {
+    showError({
+      statusCode: error.value.statusCode ?? 500,
+      message: error.value.data?.message ?? error.value.message,
+    });
+  }
+</script>
 
 <template>
-  <div class="oferte"></div>
+  <div class="oferte">
+    <UIPrettyPrint v-if="data" :data="data" />
+  </div>
 </template>
