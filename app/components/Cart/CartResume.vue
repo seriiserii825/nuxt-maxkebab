f57238b6a901327d53811118cb14942044bf4bf9
seriiserii825 <seriiburduja@gmail.com>
2026-04-13 09:19:48 +0300

    feat: start cart resume

--- app/components/Cart/CartResume.vue
+++ app/components/Cart/CartResume.vue
@@ -1,32 +1,10 @@
-<script setup lang="ts">
-  const products = [
-    { id: 1, name: "Burger pui", price: 80, image: "/img/burger-pui.jpg" },
-    { id: 2, name: "Aripioare picante", price: 79, image: "/img/aripoare-picante.jpg" },
-    { id: 3, name: "Cartofi pai", price: 40, image: "/img/cartofi-pai.jpg" },
-    { id: 4, name: "Coca-Cola", price: 27, image: "/img/coca-cola.png" },
-  ];
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="cart-resume">
     <h2 class="cart-resume__title">S-ar putea să te intereseze…</h2>
     <div class="cart-resume__grid">
-      <div v-for="product in products" :key="product.id" class="cart-resume__item">
-        <div class="cart-resume__thumb">
-          <img :src="product.image" :alt="product.name" />
-        </div>
-        <div class="cart-resume__info">
-          <div class="cart-resume__name">{{ product.name }}</div>
-          <div class="cart-resume__footer">
-            <div class="cart-resume__price">
-              {{ product.price }}<span>Lei</span>
-            </div>
-            <div class="cart-resume__add">
-              <button class="cart-resume__add-btn" type="button"><span>+</span></button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <!-- <ProductCard v-for="product in products" :key="product.id" :product="product" /> -->
     </div>
   </div>
 </template>
