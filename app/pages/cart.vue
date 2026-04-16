f9147d66144b4fa7028edcf218fb2b7dd7eaed1d
serii burduja <seriiburduja@gmail.com>
2026-04-16 18:22:53 +0300

    feat: cart empty with translations

--- app/pages/cart.vue
+++ app/pages/cart.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
   const { t } = useI18n();
+  const cartStore = useCartStore();
 </script>
 
 <template>
   <div class="page-cart">
     <div class="container">
       <h1 class="page-cart__title">{{ t("cart.title") }}</h1>
-      <CartForm />
-      <div class="page-cart__collaterals">
-        <CartResume />
-        <CartTotal />
-      </div>
+      <template v-if="cartStore.count === 0">
+        <p class="page-cart__empty">{{ t("cart.empty") }}</p>
+        <NuxtLink to="/" class="page-cart__back">{{ t("cart.backToShop") }}</NuxtLink>
+      </template>
+      <template v-else>
+        <CartForm />
+        <div class="page-cart__collaterals">
+          <CartResume />
+          <CartTotal />
+        </div>
+      </template>
     </div>
   </div>
 </template>
